c9113ff8b1b9350896ef111f3be15fbfdfc0eb1c
Andres6936 <joan.buritica@artemisa.tech>
2022-08-20 19:31:18 -0500

    Moved Code

--- src/CanvasContext.ts
+++ src/CanvasContext.ts
@@ -24,6 +24,13 @@ function createCanvasCtx(width: number, height: number): CanvasContext {
     return {canvas, ctx};
 }
 
+function addFilter(srcCanvas: HTMLCanvasElement, filter: string): HTMLCanvasElement {
+    let {canvas, ctx}: CanvasContext = createCanvasCtx(srcCanvas.width, srcCanvas.height);
+    ctx.filter = filter;
+    ctx.drawImage(srcCanvas, 0, 0);
+    return canvas;
+}
+
 /**
  * Convert data to image according to callback function
  * @param {any[]} values
@@ -42,9 +49,17 @@ function data2image(values: Float32Array, width: number, converter: (v: number, 
     return canvas;
 }
 
-function addFilter(srcCanvas: HTMLCanvasElement, filter: string): HTMLCanvasElement {
-    let {canvas, ctx}: CanvasContext = createCanvasCtx(srcCanvas.width, srcCanvas.height);
-    ctx.filter = filter;
-    ctx.drawImage(srcCanvas, 0, 0);
-    return canvas;
+/**
+ * Returns alpha channel of the image as numbers in 0-255 range.
+ * @param {HTMLCanvasElement} canvas
+ * @returns {values:Float32Array[]}
+ */
+function image2alpha(canvas: HTMLCanvasElement): Float32Array {
+    let ctx: CanvasRenderingContext2D = context2d(canvas);
+    let idata: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let data: Uint8ClampedArray = idata.data;
+    let values = new Float32Array(data.length / 4);
+    for (let i = 0; i < data.length; i++) values[i] = data[i * 4 + 3] / 255;
+
+    return values;
 }
