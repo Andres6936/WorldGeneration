e9cbf15ffdd9bb306d00cdb09a425abdcdf4a736
Andres6936 <joan.buritica@artemisa.tech>
2022-08-23 20:52:35 -0500

    Moved Code

--- src/UtilImage.ts
+++ src/UtilImage.ts
@@ -24,3 +24,50 @@ function subImage(image: HTMLCanvasElement, left: number, top: number, width: nu
     ctx.drawImage(image, left, top, width, height, 0, 0, width, height);
     return canvas;
 }
+
+function colorFromRGBString(color) {
+    let n = parseInt(color, 16);
+    return [Math.floor(n / 65536), Math.floor(n / 256) % 256, n % 256, 256];
+}
+
+/**
+ * Returns elevation image with higher elevation being brighter
+ */
+function elevation2Image(
+    {elevation, rivers},
+    {
+        discreteHeights = 10,
+        terrainTypeColoring = false,
+        hillRatio = 0.1,
+        mountainRatio = 0.02,
+        green = true,
+    }
+) {
+    let hillElevation = approximateQuantile(elevation, 1 - hillRatio);
+    let mountainElevation = approximateQuantile(elevation, 1 - mountainRatio);
+
+    return (v, i) => {
+        if (rivers[i] && v > 0) {
+            return [0, v * 400, 200, 255];
+        }
+
+        let level = discreteHeights
+            ? Math.floor(v * discreteHeights) / discreteHeights
+            : v;
+
+        if (v > 0) {
+            if (terrainTypeColoring)
+                return v < hillElevation
+                    ? [32, 128, 32, 255]
+                    : v < mountainElevation
+                        ? [196, 196, 32, 255]
+                        : [128, 32, 0, 255];
+            else
+                return green
+                    ? [level * 400, level * 150 + 100, 50, 255]
+                    : [250 - level * 300, 200 - level * 300, 0, 255];
+        } else {
+            return [0, level * 60 + 60, level * 80 + 100, 255];
+        }
+    };
+}
--- worldgen.ts
+++ worldgen.ts
@@ -476,10 +476,6 @@ function mapToList(m) {
   return l;
 }
 
-function colorFromRGBString(color) {
-  let n = parseInt(color, 16);
-  return [Math.floor(n / 65536), Math.floor(n / 256) % 256, n % 256, 256];
-}
 
 const contrastColors = mapToList({
   [DESERT]: "ffff00",
@@ -499,47 +495,6 @@ const contrastColors = mapToList({
   [BEACH]: "ffff80",
 }).map(colorFromRGBString);
 
-/**
- * Returns elevation image with higher elevation being brighter
- */
-function elevation2Image(
-  { elevation, rivers },
-  {
-    discreteHeights = 10,
-    terrainTypeColoring = false,
-    hillRatio = 0.1,
-    mountainRatio = 0.02,
-    green = true,
-  }
-) {
-  let hillElevation = approximateQuantile(elevation, 1 - hillRatio);
-  let mountainElevation = approximateQuantile(elevation, 1 - mountainRatio);
-
-  return (v, i) => {
-    if (rivers[i] && v > 0) {
-      return [0, v * 400, 200, 255];
-    }
-
-    let level = discreteHeights
-      ? Math.floor(v * discreteHeights) / discreteHeights
-      : v;
-
-    if (v > 0) {
-      if (terrainTypeColoring)
-        return v < hillElevation
-          ? [32, 128, 32, 255]
-          : v < mountainElevation
-          ? [196, 196, 32, 255]
-          : [128, 32, 0, 255];
-      else
-        return green
-          ? [level * 400, level * 150 + 100, 50, 255]
-          : [250 - level * 300, 200 - level * 300, 0, 255];
-    } else {
-      return [0, level * 60 + 60, level * 80 + 100, 255];
-    }
-  };
-}
 
 
 
