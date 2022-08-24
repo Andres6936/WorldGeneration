b2cf3ba2140e67be15873c702a2b4ad56c61fa55
Andres6936 <joan.buritica@artemisa.tech>
2022-08-23 21:07:05 -0500

    Moved Code

--- src/HexDraw.ts
+++ src/HexDraw.ts
@@ -17,7 +17,7 @@ function createCanvasCtx(width, height) {
   canvas.width = width;
   canvas.height = height;
   let ctx = context2d(canvas);
-  return { canvas, ctx };
+  return {canvas, ctx};
 }
 
 /**
@@ -29,9 +29,9 @@ function createMask(size = 32, layout) {
   let sectorsNumber = layout == SQUARE ? 8 : 12;
 
   let canvases = [...Array(sectorsNumber + 1)].map(() => {
-    let { canvas, ctx } = createCanvasCtx(size, size);
+    let {canvas, ctx} = createCanvasCtx(size, size);
     let data = ctx.createImageData(size, size);
-    return { canvas, data };
+    return {canvas, data};
   });
 
   for (let x = 0; x < size; x++)
@@ -39,18 +39,18 @@ function createMask(size = 32, layout) {
       let point = (y * size + x) * 4;
       let angle = Math.atan2(size / 2 - 0.5 - x, y - (size / 2 - 0.5));
       let sector =
-        sectorsNumber == 12
-          ? Math.floor((angle / Math.PI + 1) * 6)
-          : (Math.floor((angle / Math.PI + 1) * 4) + 1) % 8;
+          sectorsNumber == 12
+              ? Math.floor((angle / Math.PI + 1) * 6)
+              : (Math.floor((angle / Math.PI + 1) * 4) + 1) % 8;
       canvases[sector].data.data.set([255, 255, 255, 255], point);
       canvases[sectorsNumber].data.data.set(
-        [
-          (sector / 12) * 255,
-          255 - (sector / 12) * 255,
-          (sector % 2) * 255,
-          255,
-        ],
-        point
+          [
+            (sector / 12) * 255,
+            255 - (sector / 12) * 255,
+            (sector % 2) * 255,
+            255,
+          ],
+          point
       );
     }
   for (let c of canvases) {
@@ -66,21 +66,21 @@ function cutImageToSectors(image, left, top, masks) {
 
   for (let part = 0; part < 3; part++) {
     for (let sector = 0; sector < masks.length - 1; sector++) {
-      let { canvas, ctx } = createCanvasCtx(r * 2, r * 2);
+      let {canvas, ctx} = createCanvasCtx(r * 2, r * 2);
 
       ctx.drawImage(masks[sector], 0, 0);
       ctx.globalCompositeOperation = "source-in";
 
       ctx.drawImage(
-        image,
-        left,
-        top + part * size,
-        size,
-        size,
-        0,
-        0,
-        size,
-        size
+          image,
+          left,
+          top + part * size,
+          size,
+          size,
+          0,
+          0,
+          size,
+          size
       );
 
       ctx.globalCompositeOperation = "source-over";
@@ -103,28 +103,28 @@ function cutImageToSectors(image, left, top, masks) {
  * @param {*} top
  */
 function cutImageUp(
-  image,
-  columns,
-  rows,
-  fragmentWidth,
-  fragmentHeight,
-  left = 0,
-  top = 0
+    image,
+    columns,
+    rows,
+    fragmentWidth,
+    fragmentHeight,
+    left = 0,
+    top = 0
 ) {
   let imagePieces = [];
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < columns; x++) {
-      let { canvas, ctx } = createCanvasCtx(fragmentWidth, fragmentHeight);
+      let {canvas, ctx} = createCanvasCtx(fragmentWidth, fragmentHeight);
       ctx.drawImage(
-        image,
-        left + x * fragmentWidth,
-        top + y * fragmentHeight,
-        fragmentWidth,
-        fragmentHeight,
-        0,
-        0,
-        fragmentWidth,
-        fragmentHeight
+          image,
+          left + x * fragmentWidth,
+          top + y * fragmentHeight,
+          fragmentWidth,
+          fragmentHeight,
+          0,
+          0,
+          fragmentWidth,
+          fragmentHeight
       );
       imagePieces.push(canvas);
     }
@@ -142,8 +142,8 @@ function cutImageUp(
  * 4 - nor horizontally or vertically, but diagonally
  */
 const partsOrder = "334433443223100110013223"
-  .split("")
-  .map((s, i) => Number(s) * 4 + (i % 2) + (Math.floor(i / 4) % 2) * 2);
+    .split("")
+    .map((s, i) => Number(s) * 4 + (i % 2) + (Math.floor(i / 4) % 2) * 2);
 
 function cutImagetoSquares(img, left, top) {
   let partsUnordered = cutImageUp(img, 4, 6, 16, 16, left, top);
@@ -176,13 +176,13 @@ function drawPath(ctx, tile, pos, columns, connect) {
   ctx.drawImage(tile[1], tileX + pixelPathDisplacement, tileY + pixelPathDisplacement);
   for (let i = 0; i < 4; i++) {
     let connected = connect(
-      pos + pathNeighbors[i][0] + pathNeighbors[i][1] * columns
+        pos + pathNeighbors[i][0] + pathNeighbors[i][1] * columns
     );
     if (connected)
       ctx.drawImage(
-        tile[2 + i],
-        tileX + tileWidth * (pathNeighbors[i][0] / 2) + pixelPathDisplacement,
-        tileY + tileWidth * (pathNeighbors[i][1] / 2) + pixelPathDisplacement
+          tile[2 + i],
+          tileX + tileWidth * (pathNeighbors[i][0] / 2) + pixelPathDisplacement,
+          tileY + tileWidth * (pathNeighbors[i][1] / 2) + pixelPathDisplacement
       );
   }
 }
@@ -198,14 +198,15 @@ function drawSquareTile(ctx, tile, pos, columns, connect) {
     let yNear = !connect(pos + dy * columns);
     let kind = 0;
     if (xNear || yNear) {
+      // @ts-ignore
       kind = xNear + yNear * 2;
     } else if (!connect(pos + dy * columns + dx)) {
       kind = 4;
     }
     ctx.drawImage(
-      tile[kind * 4 + corner],
-      tileX + (corner % 2 ? 16 : 0),
-      tileY + (corner > 1 ? 16 : 0)
+        tile[kind * 4 + corner],
+        tileX + (corner % 2 ? 16 : 0),
+        tileY + (corner > 1 ? 16 : 0)
     );
   }
 }
@@ -240,7 +241,7 @@ function drawTile(ctx, tile, pos, layout, columns, neighborDeltas, connect) {
     let imagei = subi;
     let side = Math.floor(subi / 2);
     let neighbor2Side =
-      (side + (subi % 2 ? 1 : sectorsNumber / 2 - 1)) % (sectorsNumber / 2);
+        (side + (subi % 2 ? 1 : sectorsNumber / 2 - 1)) % (sectorsNumber / 2);
     let neighbor1 = connect(pos + deltas[side]);
     let neighbor2 = connect(pos + deltas[neighbor2Side]);
     let mode = neighbor1 ? 2 : neighbor2 ? 1 : 0;
@@ -260,37 +261,37 @@ function createSprites(tileset, layout) {
   for (let layer of tileset.connected) {
     let ispath = layer[3] & ISPATH ? true : false;
     let slices =
-      layout == SQUARE
-        ? ispath
-          ? cutImageUp(
-              tileset.tilesheet,
-              2,
-              3,
-              32,
-              32,
-              layer[1] * tileSize,
-              layer[2] * tileSize
-            )
-          : cutImagetoSquares(
-              tileset.tilesheet,
-              layer[1] * tileSize,
-              layer[2] * tileSize
-            )
-        : cutImageToSectors(
-            tileset.tilesheet,
-            layer[1] * tileSize,
-            layer[2] * tileSize,
-            masks
-          );
+        layout == SQUARE
+            ? ispath
+                ? cutImageUp(
+                    tileset.tilesheet,
+                    2,
+                    3,
+                    32,
+                    32,
+                    layer[1] * tileSize,
+                    layer[2] * tileSize
+                )
+                : cutImagetoSquares(
+                    tileset.tilesheet,
+                    layer[1] * tileSize,
+                    layer[2] * tileSize
+                )
+            : cutImageToSectors(
+                tileset.tilesheet,
+                layer[1] * tileSize,
+                layer[2] * tileSize,
+                masks
+            );
     sprites[layer[0]] = slices;
   }
   for (let tile of tileset.single) {
     let image = subImage(
-      tileset.tilesheet,
-      tile[1] * tileSize,
-      tile[2] * tileSize,
-      tileSize,
-      tileSize
+        tileset.tilesheet,
+        tile[1] * tileSize,
+        tile[2] * tileSize,
+        tileSize,
+        tileSize
     );
     sprites[tile[0]] = image;
   }
@@ -342,22 +343,23 @@ function drawTerrain(ctx, grid, directional, columns, tileset, layout) {
       for (let layer of grid[ind]) {
         if (connected[layer]) {
           drawTile(
-            ctx,
-            sprites[layer],
-            ind,
-            layout,
-            columns,
-            neighborDeltas,
-            (neighbor) => {
-              if (!(bits[neighbor] & (1 << idToGroup[layer]))) return false;
-              if (!(layer in directional)) return true;
-              return (
-                directional[layer][neighbor] == ind ||
-                directional[layer][ind] == neighbor
-              );
-            }
+              ctx,
+              sprites[layer],
+              ind,
+              layout,
+              columns,
+              neighborDeltas,
+              (neighbor) => {
+                if (!(bits[neighbor] & (1 << idToGroup[layer]))) return false;
+                if (!(layer in directional)) return true;
+                return (
+                    directional[layer][neighbor] == ind ||
+                    directional[layer][ind] == neighbor
+                );
+              }
           );
         } else {
+          // @ts-ignore
           drawTile(ctx, sprites[layer], ind, layout, columns);
         }
       }
