47805ba86e06b6d7a2407b788207c84e3610c11c
Andres6936 <andres6936@gmail.com>
2025-01-29 21:23:48 -0500

    Extract inline styles to external CSS file
    Moved inline styles in `index.html` to a new `index.css` file for better maintainability and separation of concerns. Updated `index.tsx` to import the new CSS file.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {Form} from "./form";
 
+import './index.css'
+
 window.tips = {};
 window.maps = [];
 window.miniMaps = [];
