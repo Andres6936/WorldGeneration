394d7a835d8f4ab536d5e0545bc08911f9ef4f23
Andres6936 <andres6936@gmail.com>
2025-02-18 10:37:31 -0500

    Add dynamic tooltip rendering with mouse tracking
    Introduced a `useEffect` hook to handle tooltip updates based on mouse movement. The tooltip now dynamically displays contextual data from maps and settings, adjusting its position and content accordingly. Cleaned up event listeners to prevent potential memory leaks.

--- src/tooltip/index.tsx
+++ src/tooltip/index.tsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import {biomeNames} from "../Mapper.ts";
+import {useMaps} from "../store/useMaps.ts";
+import {useSettings} from "../store/useSettings.ts";
 
 export const Tooltip = React.memo(() => {
+    const maps = useMaps(state => state.maps);
+    const settings = useSettings(state => state.settings);
+
+    const tooltipAt = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (tooltipAt.current === null || maps === null) return;
+        const tooltip = tooltipAt.current;
+
+        const handleMouseMove = (e: MouseEvent) => {
+            let target = e.target;
+
+            tooltip.style.left = Math.min(window.innerWidth - 300, e.screenX + 20).toString();
+            tooltip.style.top = Math.min(window.innerHeight - 200, e.screenY - 40).toString();
+
+            let isCanvas = e.target.tagName == "CANVAS";
+            let id = e.target.id;
+            tooltip.style.display = isCanvas ? "grid" : window.tips[id] ? "block" : "none";
+
+            if (isCanvas) {
+                let localX = (e.offsetX / target.width) * settings.width;
+                let localY = (e.offsetY / target.height) * settings.height;
+                let ind = Math.floor(localX) + Math.floor(localY) * settings.width;
+                tooltip.innerHTML = Object.keys(maps)
+                    .map((key) =>
+                        key == "photo"
+                            ? ""
+                            : `<div>${key}</div><div>${
+                                key == "biome"
+                                    ? biomeNames[maps[key][ind]].toUpperCase()
+                                    : maps[key][ind]
+                            }</div>`
+                    )
+                    .join("");
+            } else if (tips[id]) {
+                tooltip.innerHTML = tips[id];
+            }
+        }
+
+        window.addEventListener('mousemove', handleMouseMove)
+
+        // Clean up the event listener on component unmount to prevent memory leaks
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove)
+        }
+    }, []);
+
     return (
-        <div id="tooltip">tooltip</div>
+        <div ref={tooltipAt} id="tooltip"/>
     )
 })
