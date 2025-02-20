4c44f4aa1601ad108562076af741a3c61bea9520
Andres6936 <andres6936@gmail.com>
2025-02-20 15:44:41 -0500

    Add Layer enum and integrate currentLayer state management
    Introduced a new Layer enum in `enums.ts` to centralize map layer definitions. Updated `useSettings` store to manage the `currentLayer` state, allowing dynamic layer selection in the application.

---
+++ src/core/enums.ts
@@ -0,0 +1,9 @@
+export const enum Layer {
+    Elevation = "Elevation",
+    Tectonics = "Tectonics",
+    Temperature = "Temperature",
+    Wind = "Wind",
+    Humidity = "Humidity",
+    Biome = "Biome",
+    Photo = "Photo",
+}
--- src/store/useSettings.ts
+++ src/store/useSettings.ts
@@ -1,21 +1,26 @@
 import {create} from "zustand";
 import {Settings} from "../global";
 import {defaultSettings} from "../settings.ts";
+import {Layer} from "../core/enums.ts";
 
 type State = {
     settings: Settings,
+    currentLayer: Layer,
     showCanvasMap: boolean,
 }
 
 type Actions = {
     setSettings: (settings: State['settings']) => void,
     setShowCanvasMap: (showCanvasMap: State['showCanvasMap']) => void,
+    setCurrentLayer: (layer: State['currentLayer']) => void,
 }
 
 export const useSettings = create<State & Actions>((set) => ({
     settings: defaultSettings,
     showCanvasMap: false,
+    currentLayer: Layer.Elevation,
     setSettings: (newSettings) => set({settings: newSettings}),
     setShowCanvasMap: (showCanvasMap) => set({showCanvasMap: showCanvasMap}),
+    setCurrentLayer: (layer) => set({currentLayer: layer}),
 }))
 
