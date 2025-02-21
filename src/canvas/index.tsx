07c20bb1b3c2e6acd6a441ded040c1bc52cf0c85
Andres6936 <andres6936@gmail.com>
2025-02-20 21:43:36 -0500

    Update CSS to use a dynamic color variable
    Replaced a hardcoded color with a CSS variable for better theming and maintainability. This change ensures consistent styling across the application.

--- src/canvas/index.tsx
+++ src/canvas/index.tsx
@@ -327,7 +327,7 @@ export const Canvas = React.memo(() => {
     }, [maps, settings]);
 
     return (
-        <div className="relative flex flex:col flex:1 bl:2px|dotted|#0F0D0E">
+        <div className="relative flex flex:col flex:1 bl:2px|dotted|$(color-gray-900)">
             <div className="absolute top:0 left:0 right:0 bottom:0 flex flex:1 overflow:scroll">
                 <canvas ref={canvasAt} id="gameMap"/>
             </div>
