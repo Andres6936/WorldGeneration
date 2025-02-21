31547d2175adfc0680ce36a2365bc69d1f496509
Andres6936 <andres6936@gmail.com>
2025-02-20 20:44:38 -0500

    Standardize button styling using CSS module.
    Replaced hardcoded class names with a CSS module for consistent styling. This improves maintainability and ensures styles are encapsulated within the component. Included the CSS module import for proper usage.

--- src/form/button-canvas-toggle.tsx
+++ src/form/button-canvas-toggle.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import {useSettings} from "../store/useSettings.ts";
 
+import styles from '../components/button/index.module.css' with {type: 'css'};
+
 export const ButtonCanvasToggle = React.memo(() => {
     const showCanvasMap = useSettings(state => state.showCanvasMap)
     const setShowCanvasMap = useSettings(state => state.setShowCanvasMap)
 
     if (showCanvasMap) {
         return (
-            <button className="mt:1rem blue" onClick={() => setShowCanvasMap(false)}>
+            <button className={styles.Button} onClick={() => setShowCanvasMap(false)}>
                 Show canvas map
             </button>
         )
     } else {
         return (
-            <button className="mt:1rem blue" onClick={() => setShowCanvasMap(true)}>
+            <button className={styles.Button} onClick={() => setShowCanvasMap(true)}>
                 Hidden canvas map
             </button>
         )
