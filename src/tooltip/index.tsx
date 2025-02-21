9a661bceaa4c02f443f733a15a6fe2c54d57d579
Andres6936 <andres6936@gmail.com>
2025-02-20 22:46:18 -0500

    Rename `tooltip` variable to `container` for clarity.
    This change improves code readability by using a more descriptive variable name. The variable `tooltip` is now renamed to `container` to better reflect its role, making the code easier to understand and maintain. No functionality is affected by this update.

--- src/tooltip/index.tsx
+++ src/tooltip/index.tsx
@@ -12,23 +12,23 @@ export const Tooltip = React.memo(() => {
 
     useEffect(() => {
         if (tooltipAt.current === null || maps === null) return;
-        const tooltip = tooltipAt.current;
+        const container = tooltipAt.current;
 
         const handleMouseMove = (e: MouseEvent) => {
             let target = e.target;
 
-            tooltip.style.left = Math.min(window.innerWidth - 300, e.screenX + 20).toString();
-            tooltip.style.top = Math.min(window.innerHeight - 200, e.screenY - 40).toString();
+            container.style.left = Math.min(window.innerWidth - 300, e.screenX + 20).toString();
+            container.style.top = Math.min(window.innerHeight - 200, e.screenY - 40).toString();
 
             let isCanvas = e.target.tagName == "CANVAS";
             let id = e.target.id;
-            tooltip.style.display = isCanvas ? "grid" : window.tips[id] ? "block" : "none";
+            container.style.display = isCanvas ? "grid" : window.tips[id] ? "block" : "none";
 
             if (isCanvas) {
                 let localX = (e.offsetX / target.width) * settings.width;
                 let localY = (e.offsetY / target.height) * settings.height;
                 let ind = Math.floor(localX) + Math.floor(localY) * settings.width;
-                tooltip.innerHTML = Object.keys(maps)
+                container.innerHTML = Object.keys(maps)
                     .map((key) =>
                         key == "photo"
                             ? ""
@@ -40,7 +40,7 @@ export const Tooltip = React.memo(() => {
                     )
                     .join("");
             } else if (tips[id]) {
-                tooltip.innerHTML = tips[id];
+                container.innerHTML = tips[id];
             }
         }
 
