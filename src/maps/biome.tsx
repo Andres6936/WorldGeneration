9122ee2f739d20ae2f430952b26cacf75e6aab19
Andres6936 <andres6936@gmail.com>
2025-03-03 22:26:00 -0500

    Update canvas element with responsive class styles
    Added responsive classes to the canvas element to ensure better adaptability and appearance across different screen sizes. This improves the layout and visual consistency.

--- src/maps/biome.tsx
+++ src/maps/biome.tsx
@@ -38,6 +38,6 @@ export const Biome = React.memo(({withReduceSize}: Props) => {
     }, [maps, settings]);
 
     return (
-        <canvas ref={drawAt}/>
+        <canvas className="h:full w:auto max-w:fit" ref={drawAt}/>
     )
 })
