270d5f6f5a357f0639983a270ef941d38a141bc7
Andres6936 <andres6936@gmail.com>
2025-03-03 22:10:07 -0500

    Add visual divider and arrow SVG to Compare component
    Introduced a new vertical divider with an embedded arrow SVG to enhance the UI of the Compare component. The divider improves visual distinction, while the arrow provides a clear indicator for user interaction or alignment.

--- src/components/compare/index.tsx
+++ src/components/compare/index.tsx
@@ -6,10 +6,20 @@ type Props = {
 
 export function Compare({children}: Props) {
     return (
-        <div className="abs top:0 left:0 right:0 bottom:0 flex flex:1 flex:col overflow:hidden z:1">
-            <div className="w:15rem overflow:hidden">
-                {children}
+        <>
+            <div className="abs top:0 left:0 right:0 bottom:0 flex flex:1 flex:col overflow:hidden z:1">
+                <div className="w:15rem overflow:hidden">
+                    {children}
+                </div>
             </div>
-        </div>
+
+            <div className="abs top:0 z:1 h:full w:2px ml:-1px bg:black left:15rem">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                     className="abs z:2 color:#333 w:5px h:5px top:50% left:15rem translate(50%,50%) p:6px b:2px|solid|currentColor r:50% bg:rgba(255,255,255,1)"
+                     viewBox="0 0 16 16">
+                    <path d="M 6 2 L 1 8 L 6 14 M 10 2 L 15 8 L 10 14" stroke="currentColor"></path>
+                </svg>
+            </div>
+        </>
     )
 }
