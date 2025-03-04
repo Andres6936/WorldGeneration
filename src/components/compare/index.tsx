b21176019067ed3b821e1dc1867f6beaf301a289
Andres6936 <andres6936@gmail.com>
2025-03-03 22:20:48 -0500

    Enhance comparison slider styling and add range input.
    Updated the SVG size and added a stroke class for better visuals. Introduced an invisible range input to enable adjustable slider functionality. This improves both usability and appearance.

--- src/components/compare/index.tsx
+++ src/components/compare/index.tsx
@@ -15,11 +15,16 @@ export function Compare({children}: Props) {
 
             <div className="abs top:0 z:1 h:full w:2px ml:-1px bg:black left:15rem">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                     className="abs z:2 color:#333 w:5px h:5px top:50% left:15rem translate(50%,50%) p:6px b:2px|solid|currentColor r:50% bg:rgba(255,255,255,1)"
+                     className="abs z:2 color:#333 w:32px h:32px top:50% left:15rem translate(50%,50%) p:6px b:2px|solid|currentColor r:50% bg:rgba(255,255,255,1)"
                      viewBox="0 0 16 16">
-                    <path d="M 6 2 L 1 8 L 6 14 M 10 2 L 15 8 L 10 14" stroke="currentColor"></path>
+                    <path d="M 6 2 L 1 8 L 6 14 M 10 2 L 15 8 L 10 14" stroke="currentColor"
+                          className="stroke:2px"></path>
                 </svg>
             </div>
+
+            <input
+                className="appearance:none abs top:0 left:calc(32px/-2) width:calc(100%+32px) h:full opacity:0 z:2 cursor:col-resize bg:transparent"
+                type="range" min="0" step="0.5" max="100" value="50"/>
         </>
     )
 }
