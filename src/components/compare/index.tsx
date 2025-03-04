cf8b99ec7b13f93c444d0411dfbc41d56e868fd4
Andres6936 <andres6936@gmail.com>
2025-03-03 22:28:43 -0500

    Add max-width style to SVG in compare component
    This ensures the SVG element respects the fit content sizing, improving alignment and preventing unintended scaling issues. It refines the component's styling for better visual consistency.

--- src/components/compare/index.tsx
+++ src/components/compare/index.tsx
@@ -15,7 +15,7 @@ export function Compare({children}: Props) {
 
             <div className="abs top:0 z:1 h:full w:2px ml:-1px bg:black left:15rem">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                     className="abs z:2 color:#333 w:32px h:32px top:50% left:15rem translate(50%,50%) p:6px b:2px|solid|currentColor r:50% bg:rgba(255,255,255,1)"
+                     className="abs z:2 color:#333 w:32px h:32px top:50% left:15rem translate(50%,50%) p:6px b:2px|solid|currentColor r:50% bg:rgba(255,255,255,1) max-w:fit"
                      viewBox="0 0 16 16">
                     <path d="M 6 2 L 1 8 L 6 14 M 10 2 L 15 8 L 10 14" stroke="currentColor"
                           className="stroke:2px"></path>
